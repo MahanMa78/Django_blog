f882a0f1f3209c1e3b86563f599968c3b2a54169
Mahan Mazaheri <mahan78ir@gmail.com>
2024-08-14 09:46:51 +0330

    edit forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -4,13 +4,13 @@ from .models import Post , Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title' , 'excerpt' , 'body'  , 'date' , 'photo'] # ma 'author' ro hazf kardim ta mostaghiman khode on kasi ke dare post ro minevise biad va post ro montasher kone
+        fields = ['title' , 'excerpt' , 'body'  , 'date' , 'photo','category' , 'tags'] # ma 'author' ro hazf kardim ta mostaghiman khode on kasi ke dare post ro minevise biad va post ro montasher kone
         
 
 class PostUpdateForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title' , 'excerpt' , 'body' , 'photo']
+        fields = ['title' , 'excerpt' , 'body' , 'photo' , 'category' , 'tags' ]
 
 
 class CommentForm(forms.ModelForm):
