e5fbafa4ae6f494325e8861c327051074ca52add
Mahan Mazaheri <mahan78ir@gmail.com>
2024-08-18 21:53:24 +0330

    add description to model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
 
 #ma ta ghabl az merge kardan commit ha bedon model baraye khodemon mitonestim yek user besazim
 #ama az inja be baad ma user ro shakhsi saz tar kardim
@@ -9,4 +10,4 @@ class CustomUser(AbstractUser):
     photo = models.ImageField(upload_to='photo/profile/%Y/%m/%d/' , default='null')
     about = models.CharField(max_length=255 , default='null') 
     #default='null' --> komak mikone ke data haye ghabli dakhel database zamani ke vojod daran dochar moshkel nashan
-    
+    description = RichTextField(default='something about me')
